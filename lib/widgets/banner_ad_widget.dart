49eb5cbe3c55dda68f1b834ef8dec2a69efb37c9
Drake <jieliangh@icloud.com>
2026-01-04 13:34:34 +0800

    refactor(ads): migrate BannerAdWidget to use BannerAdCubit

--- lib/widgets/banner_ad_widget.dart
+++ lib/widgets/banner_ad_widget.dart
@@ -1,69 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:garage/core/core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'bloc/banner_ad/banner_ad_cubit.dart';
+import 'bloc/banner_ad/banner_ad_state.dart';
 
-class BannerAdWidget extends StatefulWidget {
+class BannerAdWidget extends StatelessWidget {
   final AdSize adSize;
 
   const BannerAdWidget({super.key, this.adSize = AdSize.banner});
 
   @override
-  State<BannerAdWidget> createState() => _BannerAdWidgetState();
-}
-
-class _BannerAdWidgetState extends State<BannerAdWidget> {
-  BannerAd? _bannerAd;
-  bool _isLoaded = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadAd();
-  }
-
-  void _loadAd() {
-    if (getIt.repo.ad.isBannerAdFree) return;
-
-    _bannerAd = BannerAd(
-      adUnitId: getIt.repo.ad.bannerAdUnitId,
-      size: widget.adSize,
-      request: const AdRequest(),
-      listener: BannerAdListener(
-        onAdLoaded: (ad) {
-          setState(() {
-            _isLoaded = true;
-          });
-        },
-        onAdFailedToLoad: (ad, error) {
-          debugPrint('BannerAd failed to load: $error');
-          ad.dispose();
-          setState(() {
-            _isLoaded = false;
-          });
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => BannerAdCubit(adSize: adSize)..loadAd(),
+      child: BlocBuilder<BannerAdCubit, BannerAdState>(
+        builder: (context, state) {
+          if (state is BannerAdLoaded) {
+            return Container(
+              width: adSize.width.toDouble(),
+              height: adSize.height.toDouble(),
+              alignment: Alignment.center,
+              child: AdWidget(ad: state.ad),
+            );
+          }
+          // Initial, Loading, Error, Hidden return empty
+          return const SizedBox.shrink();
         },
       ),
-    );
-
-    _bannerAd!.load();
-  }
-
-  @override
-  void dispose() {
-    _bannerAd?.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (getIt.repo.ad.isBannerAdFree || _bannerAd == null || !_isLoaded) {
-      return const SizedBox.shrink();
-    }
-
-    return Container(
-      width: widget.adSize.width.toDouble(),
-      height: widget.adSize.height.toDouble(),
-      alignment: Alignment.center,
-      child: AdWidget(ad: _bannerAd!),
     );
   }
 }
